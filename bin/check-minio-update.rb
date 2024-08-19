8b35ee32628f248152b0d62ee1138bf1f4429844
Markus Benkenstein <markus.benkenstein@aboutsource.net>
2024-08-19 13:37:15 +0200

    linting: no possibility to shorten the line

--- bin/check-minio-update.rb
+++ bin/check-minio-update.rb
@@ -58,7 +58,7 @@ class CheckMinioUpdate < Sensu::Plugin::Check::CLI
 
   def latest_version
     @latest_version ||= begin
-      uri = URI.parse("#{config[:checkurl]}/#{config[:platform]}/minio.sha256sum")
+      uri = URI.parse("#{config[:checkurl]}/#{config[:platform]}/minio.sha256sum") # rubocop:disable Layout/LineLength
       response = Net::HTTP.get_response(uri)
 
       unless response.is_a?(Net::HTTPSuccess)
